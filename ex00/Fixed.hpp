d9d75c34474a10f1b034541da6b62671734f26a5
nicolasgsantana <nicolas07san@gmail.com>
2026-08-24 15:13:25 -0300

    feat: finish ex00

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -1,6 +1,8 @@
 #ifndef FIXED_HPP
 # define FIXED_HPP
 
+# include <iostream>
+
 class	Fixed
 {
 private:
@@ -10,6 +12,10 @@ public:
 	Fixed(void);
 	Fixed(const Fixed &obj);
 	Fixed &operator= (const Fixed &obj);
-}
+	~Fixed(void);
+
+	int		getRawBits(void) const;
+	void	setRawBits(const int raw);
+};
 
 #endif
